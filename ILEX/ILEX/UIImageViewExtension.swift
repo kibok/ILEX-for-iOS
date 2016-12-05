7969f76cbc1dc36991b0cf9dbe00441dc91f1d3b
matsushita <matsushita@fenrir.co.jp>
2016-12-05 23:07:06 +0900

    商品画像スケールを修正

--- ILEX/ILEX/UIImageViewExtension.swift
+++ ILEX/ILEX/UIImageViewExtension.swift
@@ -24,7 +24,7 @@ extension UIImageView {
             }.resume()
     }
     
-    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFill) {
         guard let url = URL(string: link) else { return }
         downloadedFrom(url: url, contentMode: mode)
     }
